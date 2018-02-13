22cd2a2bbdc2bed5194197db5d46f02a331daa50
Nico Noster <niconoster@t-online.de>
2018-02-13 20:06:15 +0100

    Remove EZGUI modul, replace with JQuery

--- js/states/game/Ui.js
+++ js/states/game/Ui.js
@@ -1,64 +1,55 @@
 // SCREENS AND OPTIONS //
-var mainScreen, mainScreen_opened = false, mainScreenJSON;
+var window_1, window_1_opened = false;
 var shop, shop_opened = false;
 
 function gui_setup() {
-  var clickObject_1 = EZGUI.components.second_tab;
+  var clickObject_1 = shop;
 
   clickObject_1.on('click', function() {
-    if(EZGUI.components.first_tab.text == 'Koordinaten: ') EZGUI.components.first_tab.text += '53/17';
-    else EZGUI.components.first_tab.text = 'Koordinaten: ' + '53/18';
-
     gui_checkShop();
   });
 }
 
 function gui_load() {
-  var gui_width = 200;
-  var gui_height = 150;
-  mainScreenJSON = loadJSON(gui_width, gui_height);
-  EZGUI.Theme.load(['modules/ezgui/assets/metalworks-theme/metalworks-theme.json'], function() {
-    mainScreen = EZGUI.create(mainScreenJSON, 'metalworks');
-    mainScreen.position.x = WIDTH/2 - gui_width/2;
-    mainScreen.position.y = HEIGHT/2 - gui_height/2;
-    mainScreen.visible = false;
-    mainScreen_opened = false;
+  window_1 = $('.gui_1')[0];
 
-    gui_setup();
-    gui_buildShop();
-  });
+  shop = $('.shop')[0]; // initialize shop
+  gui_build_Window1();
+  gui_build_Shop();
 }
 
 function gui_check() {
-  if(!mainScreen_opened) {
-    mainScreen.visible = true;
-    mainScreen_opened = true;
+  if(!window_1_opened) {
+    $(window_1).show();
+    window_1_opened = true;
   } else {
-    mainScreen.visible = false;
-    mainScreen_opened = false;
+    $(window_1).hide();
+    window_1_opened = false;
   }
 }
 
-function gui_buildShop() {
+function gui_build_Window1() {
+  var window_1_width = GAME.scale.width/2;
+  var window_1_height = GAME.scale.height/4;
+  var window_1_left = GAME.scale.offset.x + GAME.scale.width/2 - window_1_width/2;
+  var window_1_bottom = GAME.scale.height/2 - window_1_height/2;
+
+  $(window_1).css('width', window_1_width);
+  $(window_1).css('height', window_1_height);
+  $(window_1).css('left', window_1_left);
+  $(window_1).css('bottom', window_1_bottom);
+}
+
+function gui_build_Shop() {
   var shop_width = GAME.scale.width;
   var shop_height = 200;
-  var shop_backgroundColor = 'red';
-  var shop_position = 'absolute';
   var shop_left = GAME.scale.offset.x;
   var shop_bottom = 0;
-  var shop_display = 'none'; // On create
 
-  shop = $('.shop')[0]; // initialize shop
-  // DEFAULT
   $(shop).css('width', shop_width);
   $(shop).css('height', shop_height);
-  $(shop).css('background-color', shop_backgroundColor);
-  // POSITION
-  $(shop).css('position', shop_position);
   $(shop).css('left', shop_left);
   $(shop).css('bottom', shop_bottom);
-  // DISPLAY
-  $(shop).css('display', shop_display);
 }
 
 function gui_resize() {
@@ -72,15 +63,5 @@ function gui_resize() {
 function gui_update() {}
 
 function gui_checkShop() {
-  // if(!shop_opened) {
-  //   $('.shop').css('display', 'block');
-  //   EZGUI.components.second_tab.text = 'Schließen';
-  //   shop_opened = true;
-  // } else {
-  //   $('.shop').css('display', 'none');
-  //   EZGUI.components.second_tab.text = 'Öffnen';
-  //   shop_opened = false;
-  // }
-console.log("sads");
   $(shop).slideToggle();
 }
