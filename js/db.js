c6f607158ff60e35a5e3c2a671b1e72711251ab7
Nico Noster <niconoster@t-online.de>
2018-02-18 17:26:53 +0100

    Connect database IE11 fix

--- js/db.js
+++ js/db.js
@@ -1,6 +1,5 @@
-//Connect
 DB.connect("toodle", true);
 //Wait for connection
-DB.ready().then(() => {
-  $(".stats_content").text("Hello Baqend");
+DB.ready().then(function() {
+  console.log("Verbunden");
 });
