a9306e89253e6bf73166cf46cc6995ee368d0675
Nico Noster <niconoster@t-online.de>
2018-03-05 02:02:36 +0100

    Registration form and Item table connected with User in Baqend

--- js/db.js
+++ js/db.js
@@ -1,10 +1,31 @@
-DB.connect("toodle", true);
+/* --- VARIABLES --- */
+var search_result = "";
+var username = "root", password = "admin";
+/* --- VARIABLES --- */
+
+DB.connect("misty-shape-74", true);
 //Wait for connection
 DB.ready().then(function() {
   console.log("Verbunden");
 });
 
-var search_field_input = $('#search_field_input');
-search_field_input.click(function() {
-  console.log(search_field_input);
-})
+
+
+function register() {
+  DB.User.register(username, password).then(function() {
+    //Hey we are logged in
+    console.log(DB.User.me.username); //'john.doe@example.com'
+  });
+}
+
+function login(username, password) {
+  DB.User.login('john.doe@example.com', 'MySecretPassword').then(function() {
+  //Hey we are logged in again
+  console.log(DB.User.me.username); //'john.doe@example.com'
+});
+}
+
+$('#search').submit(function(e) {
+  e.preventDefault();
+  search_result = $('#search_field').val();
+});
